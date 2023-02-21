b9660736c841bc9b93f32d383bfad88166566c58
unknown <butwhyilove@gmail.com>
2023-02-21 23:13:16 +0300

    1

---
+++ zadacha3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+Console.Clear();
+int[] arr = new int[8];
+Random rand = new Random();
+Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(20);
+    Console.Write(" ");
+    Console.Write(arr[i]);
+}
+Console.Write("]");
